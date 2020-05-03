d087718fc973bcbb9237c04937c2672fd2757f00
Ing. Manzanilla <al045356@uacam.mx>
2020-05-02 23:16:56 -0500

    2.0 com

--- src/ejrmi/Registro.java
+++ src/ejrmi/Registro.java
@@ -9,9 +9,6 @@ hola desde netbeans
 hola desde jeibraint
 hola 2020
 
-estas aqui
-
-estas aqui 2
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import javax.swing.JFrame;
