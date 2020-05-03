890bbbb97064d609b895a35308d679872311209b
EduardoManzanilla <al050019@uacam.mx>
2020-05-03 00:22:43 -0500

    5

--- src/ejrmi/Registro.java
+++ src/ejrmi/Registro.java
@@ -2,8 +2,14 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ * dd
+ * comit
+ *pull
+ * */
 package ejrmi;
+...
+...
+
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
