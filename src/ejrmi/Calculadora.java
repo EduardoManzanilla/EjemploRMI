43d1916064d4887291087cc53ef456abbc9d4c0e
EduardoManzanilla <al050019@uacam.mx>
2020-05-03 23:20:50 -0500

    Modulo registro listo2

--- src/ejrmi/Calculadora.java
+++ src/ejrmi/Calculadora.java
@@ -15,8 +15,8 @@ import java.rmi.RemoteException;
 public interface Calculadora extends Remote{
     public int div(int a, int b) throws RemoteException;
     public int mul(int a, int b) throws RemoteException;
-    public int sub(int a, int b) throws RemoteException;
-    public String add(String nombre, int edad, float peso, float estatura, String usuario, String contraseña) throws RemoteException;
+    public boolean sub(String user) throws RemoteException;
+    public boolean add(String nombre, int edad, float peso, float estatura, String usuario, String contraseña) throws RemoteException;
     public String log(String user, String password) throws RemoteException;
     
 }
